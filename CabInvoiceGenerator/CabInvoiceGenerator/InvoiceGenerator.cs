67ae960f0e8e89c7035daa921595749dff11d4da
sayalikeche <kechesayali11@gmail.com>
2021-12-11 08:29:42 +0530

    [sayali Added a CabInvoiceGenerator]

---
+++ CabInvoiceGenerator/CabInvoiceGenerator/InvoiceGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class InvoiceGenerator
+    {
+        readonly private double DIS;
+        readonly private double TIME;
+        readonly private double MINFARE = 5.0;
+        readonly private double COSTPERKM = 10.0;
+        readonly private double COSTPERMIN = 1.0;
+        public InvoiceGenerator(double runningDistance, double runningTime)
+        {
+            this.Dis = runningDistance;
+            this.TIME = runningTime;
+        }
+
+        public double Dis { get; }
+
+        public double CalculateCabFare()
+        {
+
+
+            double TotalFare = (DIS * COSTPERKM) + (TIME * COSTPERMIN);
+            if (TotalFare < MINFARE)
+            {
+                return MINFARE;
+            }
+            else
+            {
+                return TotalFare;
+            }
+        }
+    }
+}
---
+++ CabInvoiceGenerator/CabInvoiceGeneratorTest/CabServicesTest.cs
@@ -0,0 +1,16 @@
+﻿using CabInvoiceGenerator;
+using NUnit.Framework;
+
+namespace CabInvoiceGeneratorTest
+{
+    public class Test
+    {
+        [Test]
+        public void GivenDistanceAndTime_WhenACalculate_shouldReturntotalFare()
+        {
+            InvoiceGenerator invoice = new InvoiceGenerator(5, 2);
+            double result = invoice.CalculateCabFare();
+            Assert.AreEqual(result, 52);
+        }
+    }
+}
